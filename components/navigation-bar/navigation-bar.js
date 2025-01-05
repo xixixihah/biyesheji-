2265ab715d2d5717190a9511f793caf9e11851ce
xixixihah <xiyuheng1@outlook.com>
2025-01-05 19:26:39 +0800

    你的提交信息

--- components/navigation-bar/navigation-bar.js
+++ components/navigation-bar/navigation-bar.js
@@ -60,17 +60,15 @@ Component({
   lifetimes: {
     attached() {
       const rect = wx.getMenuButtonBoundingClientRect()
-      wx.getSystemInfo({
-        success: (res) => {
-          const isAndroid = res.platform === 'android'
-          const isDevtools = res.platform === 'devtools'
-          this.setData({
-            ios: !isAndroid,
-            innerPaddingRight: `padding-right: ${res.windowWidth - rect.left}px`,
-            leftWidth: `width: ${res.windowWidth - rect.left }px`,
-            safeAreaTop: isDevtools || isAndroid ? `height: calc(var(--height) + ${res.safeArea.top}px); padding-top: ${res.safeArea.top}px` : ``
-          })
-        }
+      const windowInfo = wx.getWindowInfo()
+      const isAndroid = windowInfo.platform === 'android'
+      const isDevtools = windowInfo.platform === 'devtools'
+      
+      this.setData({
+        ios: !isAndroid,
+        innerPaddingRight: `padding-right: ${windowInfo.windowWidth - rect.left}px`,
+        leftWidth: `width: ${windowInfo.windowWidth - rect.left}px`,
+        safeAreaTop: isDevtools || isAndroid ? `height: calc(var(--height) + ${windowInfo.safeArea.top}px); padding-top: ${windowInfo.safeArea.top}px` : ``
       })
     },
   },
