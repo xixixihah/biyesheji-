845304aa72f67d1a75e79ff617b627585a15d594
xixixihah <xiyuheng1@outlook.com>
2025-01-05 19:46:53 +0800

    用户信息1

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,21 +1,46 @@
 Page({
   data: {
     hasUserInfo: false,
-    userInfo: {
-      avatarUrl: '/images/default-avatar.png',
-      nickName: '未登录'
-    }
+    userInfo: {}
   },
 
   onLoad() {
-    // 检查是否已经登录
+    // 检查用户是否已经授权
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: (res) => {
+              this.setData({
+                userInfo: res.userInfo,
+                hasUserInfo: true
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
-  onChooseAvatar(e) {
-    const { avatarUrl } = e.detail 
-    this.setData({
-      'userInfo.avatarUrl': avatarUrl,
-      hasUserInfo: true
-    })
+  getUserProfile() {
+    console.log('getUserProfile called');
+    wx.getUserProfile({
+      desc: '展示用户信息',
+      success: (res) => {
+        console.log('User info:', res.userInfo);
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        });
+      },
+      fail: (err) => {
+        console.error('Failed to get user profile:', err);
+        wx.showToast({
+          title: '获取用户信息失败',
+          icon: 'none'
+        });
+      }
+    });
   }
-}) 
+})
